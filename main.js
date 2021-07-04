cf1ce3d08cac2a0f4c6fa1afaa6b64d8d1ba9c9f
JvPortilho <jvportilho321@gmail.com>
2021-07-04 20:25:45 -0300

    Principal
    Aqui será feito as funções , variáveis, e será adicionado o áudio.

---
+++ main.js
@@ -0,0 +1,63 @@
+
+var minutes = 25;
+var seconds = "00";
+
+
+var click_sound = new Audio("click.mp3");
+var bell = new Audio("bell.mp3");
+
+
+function template() {
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}
+
+function start_timer() {
+  click_sound.play();
+
+  
+  minutes = 24;
+  seconds = 59;
+
+
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+
+  var minutes_interval = setInterval(minutesTimer, 60000);
+  var seconds_interval = setInterval(secondsTimer, 1000);
+
+
+  function minutesTimer() {
+    minutes = minutes - 1;
+    document.getElementById("minutes").innerHTML = minutes;
+  }
+
+  
+  function secondsTimer() {
+    seconds = seconds - 1;
+    document.getElementById("seconds").innerHTML = seconds;
+
+   
+    if (seconds <= 0) {
+      if (minutes <= 0) {
+     
+        clearInterval(minutes_interval);
+        clearInterval(seconds_interval);
+
+        
+        document.getElementById("done").innerHTML =
+          "Finalizado!";
+
+       
+        document.getElementById("done").classList.add("show_message");
+
+       
+        bell.play();
+      }
+
+      
+      seconds = 60;
+    }
+  }
+}
